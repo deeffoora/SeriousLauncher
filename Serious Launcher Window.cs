f248169fa7fecd7a964de8da701499acec04272b
Иванов Сергей Васильевич <deeffoora@gmail.com>
2024-05-30 10:52:07 +0300

    requestedExecutionLevel fo directory Program Files

--- Serious Launcher Window.cs
+++ Serious Launcher Window.cs
@@ -7,24 +7,53 @@ namespace SeriousLauncher {
         private readonly string pathToSeriousTroubleSubKey = @"SOFTWARE\deeffoora-games\Serious Trouble\";
         private readonly string templateAppLocationKey = @"Application location";
         private readonly string applicationFileName = @"Serious Trouble.exe";
+        private readonly string applicationDirectoryName = @"Serious Trouble";
         private string pathToApplication = string.Empty;
 
         private void SeriousLauncherWindow_Shown(object sender, EventArgs e) {
-            bool isInstalled = CheckLocalInstalledApplication();
+            //bool isInstalled = CheckRegistry();
+            bool isInstalled = false;
+            //if (isInstalled) {
+            //    StatusStripLabel.Text = string.Format("Application installed by path '{0}'", this.pathToApplication);
+            //    return;
+            //}
+            if (this.pathToApplication == string.Empty) {
+                isInstalled = CheckFileSystem();
+            }
             if (isInstalled) {
                 StatusStripLabel.Text = string.Format("Application installed by path '{0}'", this.pathToApplication);
-            } else {
-                if (this.pathToApplication == string.Empty) {
-                    this.pathToApplication = @"C:\Users\deeffoora\Downloads\Temp";
-                }
-                SetupButton.Enabled = true;
+                return;
             }
+            if (this.pathToApplication == string.Empty) {
+                this.pathToApplication = @$"C:\ProgrammFiles\{this.applicationDirectoryName}";
+            }
+            SetupButton.Enabled = true;
         }
 
-        private bool CheckLocalInstalledApplication() {
-            RegistryKey? subKey = Registry.CurrentUser.OpenSubKey(this.pathToSeriousTroubleSubKey);
+        private bool CheckFileSystem() {
+            List<string> paths = new() {
+                @$"C:\Program Files\{this.applicationDirectoryName}",
+                @$"D:\Games\{this.applicationDirectoryName}"
+            };
+
+            foreach (var path in paths) {
+                if (Directory.Exists(path)) {
+                    this.pathToApplication = path;
+                    string filePath = Path.GetFullPath(Path.Combine(path, this.applicationFileName));
+                    if (File.Exists(filePath)) {
+                        return true;
+                    }
+                }
+            }
+            StatusStripLabel.Text = string.Format("No installation found in file system");
+            return false;
+        }
+
+        private bool CheckRegistry() {
+            RegistryKey? subKey = Registry.CurrentUser.OpenSubKey(this.pathToSeriousTroubleSubKey,
+                System.Security.AccessControl.RegistryRights.ReadKey);
             if (subKey == null) {
-                StatusStripLabel.Text = "Serious Trouble registry key is missing";
+                StatusStripLabel.Text = "Application registry key is missing";
                 return false;
             }
             string name = GetKeyByTemplateName(subKey, this.templateAppLocationKey);
@@ -41,18 +70,20 @@ namespace SeriousLauncher {
             // The last byte [00] in the sequence causes an exception
             string trimSequence = Encoding.UTF8.GetString(sequence, 0, sequence.Length - 1);
             string supposedPath = Path.GetFullPath(trimSequence);
-            if (Directory.Exists(supposedPath) == false) {
-                string root = Path.GetFullPath(Path.Combine(supposedPath, @"..\"));
-                if (Directory.Exists(root) == false) {
-                    StatusStripLabel.Text = string.Format("The path does not exist '{0}'", supposedPath);
-                    return false;
-                }
+            string root = Path.GetFullPath(Path.Combine(supposedPath, @"..\"));
+            if (Directory.Exists(root) == false) {
+                StatusStripLabel.Text = string.Format("The path does not exist '{0}'", supposedPath);
+                return false;
             }
             // The suggested installation path that is found in the registry
             this.pathToApplication = supposedPath;
+            if (Directory.Exists(supposedPath) == false) {
+                StatusStripLabel.Text = string.Format("The path does not exist '{0}'", supposedPath);
+                return false;
+            }
             string filePath = Path.GetFullPath(Path.Combine(supposedPath, this.applicationFileName));
             if (File.Exists(filePath) == false) {
-                StatusStripLabel.Text = string.Format("Application is not installed");
+                StatusStripLabel.Text = string.Format("Installation files are missing by '{0}'", supposedPath);
                 return false;
             }
             return true;
